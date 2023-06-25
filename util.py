e310ebb78b5f3c0c4a8937fb5733e37213bf8eab
Morteza <mortezamirzai120@gmail.com>
2023-06-25 13:52:52 +0330

    remove get_flops(.) function in util.py

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import re
 import torch
 import shutil
 import logging
-import torchscan
+# import torchscan
 import numpy as np
 from os.path import join
 from sklearn.decomposition import PCA
@@ -11,12 +11,12 @@ from sklearn.decomposition import PCA
 import datasets_ws
 
 
-def get_flops(model, input_shape=(480, 640)):
-    """Return the FLOPs as a string, such as '22.33 GFLOPs'"""
-    assert len(input_shape) == 2, f"input_shape should have len==2, but it's {input_shape}"
-    module_info = torchscan.crawl_module(model, (3, input_shape[0], input_shape[1]))
-    output = torchscan.utils.format_info(module_info)
-    return re.findall("Floating Point Operations on forward: (.*)\n", output)[0]
+# def get_flops(model, input_shape=(480, 640)):
+#     """Return the FLOPs as a string, such as '22.33 GFLOPs'"""
+#     assert len(input_shape) == 2, f"input_shape should have len==2, but it's {input_shape}"
+#     module_info = torchscan.crawl_module(model, (3, input_shape[0], input_shape[1]))
+#     output = torchscan.utils.format_info(module_info)
+#     return re.findall("Floating Point Operations on forward: (.*)\n", output)[0]
 
 
 def save_checkpoint(args, state, is_best, filename):
